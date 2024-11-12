f34d47e82fe432f93096200eb0d332cd37f67f24
ArsonivAlt <c-h09@outlook.com>
2024-11-13 12:36:57 +1300

    leaderboard (kinda)

--- api/getAllPlayers.js
+++ api/getAllPlayers.js
@@ -8,17 +8,15 @@ const pool = new Pool({
 export default async (req, res) => {
     await pool.connect();
 
-    const {userName} = req.body;
-
     const response = await pool.query(
-        "select username, elo from userdata where username = $1",
+        "select username, elo from userdata",
         [userName]
     )
 
     if (response.rows.length > 0) {
         const data = response.rows[0];
 
-        res.status(201).send({"alert": 0, "result": data.rows})
+        res.status(201).send({"alert": 0, "result": data})
     }else {
         res.status(301).send({"alert": 1, "result": "Could not find user, please try again."})
     }
