a8be1ecbf7a45fa39713e4602cb9002a2659580b
ArsonivAlt <c-h09@outlook.com>
2024-11-13 11:50:51 +1300

    small change

--- public/player/main.js
+++ public/player/main.js
@@ -15,5 +15,5 @@ async function getPlayerInfo() {
     const data = await response.json();
 
     console.innerText = "";
-    contentBox.innerText = data;
+    contentBox.innerText = JSON.stringify(data.result);
 }
