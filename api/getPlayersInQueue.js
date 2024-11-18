c7e9b5d6e599a178efe309b45ba237340d5a26da
ArsonivAlt <c-h09@outlook.com>
2024-11-18 20:31:11 +1300

    added playerS in queue info placeholder

--- api/getPlayersInQueue.js
+++ api/getPlayersInQueue.js
@@ -11,14 +11,13 @@ module.exports = async (req, res) => {
     const client = await pool.connect();
 
     const response = await client.query(
-        "SELECT username FROM queue",
-        [userName]
+        "SELECT * FROM queue"
     );
 
     if (response.rows.length > 0) {
-        const data = response.rows[0];
+        const data = response.rows;
 
-        res.status(201).send({"alert": 0, "result": data})
+        res.status(200).send({"alert": 0, "result": data})
     }else {
         res.status(201).send({"alert": 1, "result": "Could not find user, please try again or register."})
     }
